01a13153f2e9e75fc383e096c48b16815139b553
alen <alen@touzi.org>
2017-07-11 18:38:57 +0800

    use nanovg draw element

--- src/container_sdl.cpp
+++ src/container_sdl.cpp
@@ -2,20 +2,18 @@
 #include "http_loader.h"
 #include "logdef.h"
 #define _USE_MATH_DEFINES
-
+#include "nanovg.h"
 #include <iostream>
 #include <math.h>
 
 using namespace litehtml;
 
-container_sdl::container_sdl(litehtml::context *context,
-                             SDL_Renderer *renderer) {
+container_sdl::container_sdl(litehtml::context *context
+                          ,struct NVGcontext *vg  ) {
     // std::cout << "#ctor\n";
     m_html_context = context;
-    m_renderer = renderer;
     m_html = NULL;
-    TTF_Init();
-    SDL_SetRenderDrawBlendMode(m_renderer, SDL_BLENDMODE_BLEND);
+    m_vg = vg;
 }
 
 container_sdl::~container_sdl(void) {}
@@ -32,72 +30,20 @@ litehtml::uint_ptr container_sdl::create_font(const litehtml::tchar_t *faceName,
     //     << ", decoration: " << decoration << std::endl;
 
     // TODO load specific fonts
-    // string_vector fonts;
-    // split_string(faceName, fonts, _t(","));
 
-    std::string fontPath = "/usr/share/fonts/truetype/droid/";
-
-    std::string fontName = "DroidSans";
-    std::string key = "DroidSans";
-
-    if (weight >= 700) {
-        fontName = "DroidSerif-Bold";
-        key = "DroidSerif-Bold";
-    }
-
-    key += "-" + std::to_string(size) + "px";
-
-    // std::cout << key << "\n" << fontName << "\n";
-
-    TTF_Font *font;
-
-    if (m_fonts[key]) {
-        font = m_fonts[key];
-    } else {
-        // fontPath.c_str() + fnt_name
-        font = TTF_OpenFont((fontPath + fontName + ".ttf").c_str(), size);
-
-        // std::cout << "[create_font] OpenFont\n";
-
-        m_fonts[key] = font;
-    }
-
-    // If there was an error in loading the font
-    if (font == nullptr) {
-        LOGE("[create_font] can't load ttf: %s", fontName.c_str());
-        LOGE("TTF_OpenFont: %s", TTF_GetError());
-        return nullptr;
-    }
-
-    int ttfStyle = TTF_STYLE_NORMAL;
-
-    if (italic == fontStyleItalic) {
-        ttfStyle = ttfStyle | TTF_STYLE_ITALIC;
-    }
-
-    if ((decoration & font_decoration_linethrough) != 0) {
-        ttfStyle = ttfStyle | TTF_STYLE_STRIKETHROUGH;
-    }
-
-    if ((decoration & font_decoration_underline) != 0) {
-        ttfStyle = ttfStyle | TTF_STYLE_UNDERLINE;
-    }
-
-    if (weight >= 700) {
-        ttfStyle = ttfStyle | TTF_STYLE_BOLD;
-    }
-
-    TTF_SetFontStyle(font, ttfStyle);
-
-    int iWidth = 0, iHeight = 0;
-    TTF_SizeText(font, _t("x"), &iWidth, &iHeight);
-
+    //int iWidth = 0, iHeight = 0;
+    //TTF_SizeText(font, _t("x"), &iWidth, &iHeight);
+    int font = nvgFindFont(m_vg, faceName);
     if (fm) {
+        float ascender;
+        float descender;
+        float lineh;
+        nvgTextMetrics(m_vg, &ascender, &descender, &lineh);
         // fixme cache this
-        fm->ascent = TTF_FontAscent(font);
-        fm->descent = TTF_FontDescent(font);
-        fm->height = TTF_FontHeight(font);
-        fm->x_height = iWidth;
+        fm->ascent = ascender;
+        fm->descent = descender;
+        fm->height = lineh;
+        fm->x_height = 0; // FIXME
         fm->draw_spaces = italic == fontStyleItalic || decoration;
 
         // std::cout << "[create_font] ascent: " << fm->ascent
@@ -127,6 +73,7 @@ int container_sdl::text_width(const litehtml::tchar_t *text,
 
     if (!font) {
         // std::cout << "[text_width](" << text << ") font: null" << std::endl;
+        LOGE("Bad font");
         return 0;
     }
 
@@ -140,22 +87,25 @@ void container_sdl::draw_text(litehtml::uint_ptr hdc,
                               litehtml::uint_ptr hFont,
                               litehtml::web_color color,
                               const litehtml::position &pos) {
-    SDL_Color sdlcolor = {color.red, color.green, color.blue, color.alpha};
-    SDL_Surface *info;
-    TTF_Font *font = (TTF_Font *)hFont;
+    struct NVGcontext *vg = (struct NVGcontext*)hdc;
+    // SDL_Color sdlcolor = {color.red, color.green, color.blue, color.alpha};
+    // SDL_Surface *info;
+    // TTF_Font *font = (TTF_Font *)hFont;
+    // LOGD("draw_text: %s %d %d", text, pos.left(), pos.bottom());
+    nvgSave(vg);
+    
+    // nvgBeginPath(vg);
+    // nvgRoundedRect(vg, 0,0, 800, 600, 3.0f);
+    // nvgFillColor(vg, nvgRGBA(255,255,255,255));
+    // nvgFill(vg);
 
-    if (!(info = TTF_RenderUTF8_Blended(font, text, sdlcolor))) {
-        // handle error here, perhaps print TTF_GetError at least
-    } else {
-        // fixme - use baseline correctly
+    nvgFontSize(vg, 24.0f);
+    nvgFontFace(vg, "sans");
+    nvgFillColor(vg, nvgRGBA(color.red, color.green, color.blue, color.alpha));
+    nvgTextAlign(vg,NVG_ALIGN_LEFT|NVG_ALIGN_MIDDLE);
+    nvgText(vg, pos.top(), pos.bottom(),text, NULL);
 
-        SDL_Texture *texture = SDL_CreateTextureFromSurface(m_renderer, info);
-        SDL_Rect src = {0, 0, info->w, info->h};
-        SDL_Rect dst = {pos.x, pos.y - (int)(pos.height * 0.5), info->w,
-                        info->h};
-        SDL_RenderCopy(m_renderer, texture, &src, &dst);
-        SDL_DestroyTexture(texture);
-    }
+    nvgRestore(vg);
 }
 
 int container_sdl::pt_to_px(int pt) {
@@ -165,7 +115,10 @@ int container_sdl::pt_to_px(int pt) {
 int container_sdl::get_default_font_size() const { return 16; }
 
 void container_sdl::draw_list_marker(litehtml::uint_ptr hdc,
-                                     const litehtml::list_marker &marker) {}
+                                     const litehtml::list_marker &marker) {
+
+     struct NVGcontext *vg = (struct NVGcontext*)hdc;
+}
 
 void container_sdl::load_image(const litehtml::tchar_t *src,
                                const litehtml::tchar_t *baseurl,
@@ -190,52 +143,129 @@ void container_sdl::get_image_size(const litehtml::tchar_t *src,
 
 void container_sdl::draw_background(litehtml::uint_ptr hdc,
                                     const litehtml::background_paint &bg) {
-    if (bg.image.empty()) {
-        SDL_Rect fillRect = {bg.clip_box.x, bg.clip_box.y, bg.clip_box.width,
-                             bg.clip_box.height};
-        SDL_SetRenderDrawColor(m_renderer, bg.color.red, bg.color.green,
-                               bg.color.blue, bg.color.alpha);
-        SDL_RenderFillRect(m_renderer, &fillRect);
+     struct NVGcontext *vg = (struct NVGcontext*)hdc;
+     LOGD("draw_background");
+     if (bg.image.empty()) {
+        // SDL_Rect fillRect = {bg.clip_box.x, bg.clip_box.y, bg.clip_box.width,
+        //                      bg.clip_box.height};
+        // SDL_SetRenderDrawColor(m_renderer, bg.color.red, bg.color.green,
+        //                        bg.color.blue, bg.color.alpha);
+        // SDL_RenderFillRect(m_renderer, &fillRect);
+        LOGD("draw_background");
+    nvgSave(vg);
+    nvgBeginPath(vg);
+    nvgRoundedRect(vg, bg.clip_box.x, bg.clip_box.y, bg.clip_box.width,bg.clip_box.height, 3.0f);
+    nvgFillColor(vg, nvgRGBA(bg.color.red, bg.color.green, bg.color.blue, bg.color.alpha));
+    //  nvgFillColor(vg, nvgRGBA(0,0,0,128));
+    nvgFill(vg);
+    nvgRestore(vg);
     } else {
         // Drawing image
-        SDL_Rect fillRect = {bg.clip_box.x, bg.clip_box.y, bg.image_size.width,
-                             bg.image_size.height};
-
-        std::string basePath = "../examples/scenes/";
-        auto image = IMG_Load((basePath + bg.image).c_str());
-        auto texture = SDL_CreateTextureFromSurface(m_renderer, image);
-
-        SDL_Rect src = {0, 0, image->w, image->h};
-        SDL_RenderCopy(m_renderer, texture, &src, &fillRect);
-
-        std::cout << "Drawing... " << bg.image.c_str() << " " << image->w << ","
-                  << image->h << "\n";
-
-        std::cout << fillRect.x << "," << fillRect.y << "," << fillRect.w << ","
-                  << fillRect.h << "\n";
-
-        SDL_DestroyTexture(texture);
-        SDL_FreeSurface(image);
+        // SDL_Rect fillRect = {bg.clip_box.x, bg.clip_box.y, bg.image_size.width,
+        //                      bg.image_size.height};
+        LOGD("draw_background image %s", bg.image.c_str());
     }
 }
 
-void container_sdl::make_url(const litehtml::tchar_t *url,
-                             const litehtml::tchar_t *basepath,
-                             litehtml::tstring &out) {}
+// void container_sdl::make_url(const litehtml::tchar_t *url,
+//                              const litehtml::tchar_t *basepath,
+//                              litehtml::tstring &out) {}
+
+void drawWindow(NVGcontext* vg, const char* title, float x, float y, float w, float h)
+{
+    float cornerRadius = 3.0f;
+    NVGpaint shadowPaint;
+    NVGpaint headerPaint;
+
+    nvgSave(vg);
+//  nvgClearState(vg);
+
+    // Window
+    nvgBeginPath(vg);
+    nvgRoundedRect(vg, x,y, w,h, cornerRadius);
+    // nvgFillColor(vg, nvgRGBA(28,30,34,192));
+    nvgFillColor(vg, nvgRGBA(255,255,255,255));
+    nvgFill(vg);
+
+    // Drop shadow
+    // shadowPaint = nvgBoxGradient(vg, x,y+2, w,h, cornerRadius*2, 10, nvgRGBA(0,0,0,128), nvgRGBA(0,0,0,0));
+    // nvgBeginPath(vg);
+    // nvgRect(vg, x-10,y-10, w+20,h+30);
+    // nvgRoundedRect(vg, x,y, w,h, cornerRadius);
+    // nvgPathWinding(vg, NVG_HOLE);
+    // nvgFillPaint(vg, shadowPaint);
+    // nvgFill(vg);
+
+    // Header
+    // headerPaint = nvgLinearGradient(vg, x,y,x,y+15, nvgRGBA(255,255,255,8), nvgRGBA(0,0,0,16));
+    // nvgBeginPath(vg);
+    // nvgRoundedRect(vg, x+1,y+1, w-2,30, cornerRadius-1);
+    // nvgFillPaint(vg, headerPaint);
+    // nvgFill(vg);
+    // nvgBeginPath(vg);
+    // nvgMoveTo(vg, x+0.5f, y+0.5f+30);
+    // nvgLineTo(vg, x+0.5f+w-1, y+0.5f+30);
+    // nvgStrokeColor(vg, nvgRGBA(0,0,0,32));
+    // nvgStroke(vg);
+
+    nvgFontSize(vg, 12.0f);
+    nvgFontFace(vg, "sans");
+    nvgTextAlign(vg,NVG_ALIGN_CENTER|NVG_ALIGN_MIDDLE);
+
+    nvgFontBlur(vg,2);
+    nvgFillColor(vg, nvgRGBA(0,0,0,255));
+    nvgText(vg, x+w/2,y+16+1, title, NULL);
+
+    // nvgFontBlur(vg,0);
+    // nvgFillColor(vg, nvgRGBA(255,0,0,160));
+    // nvgText(vg, x+w/2,y+16, title, NULL);
+
+    nvgRestore(vg);
+}
+
 
 void container_sdl::draw_borders(litehtml::uint_ptr hdc,
                                  const litehtml::borders &borders,
                                  const litehtml::position &draw_pos,
                                  bool root) {
-    if (borders.top.width != 0 &&
-        borders.top.style > litehtml::border_style_hidden) {
-        SDL_Rect fillRect = {draw_pos.x, draw_pos.y, draw_pos.width,
-                             draw_pos.height};
-        SDL_SetRenderDrawColor(m_renderer, borders.top.color.red,
-                               borders.top.color.green, borders.top.color.blue,
-                               borders.top.color.alpha);
-        SDL_RenderDrawRect(m_renderer, &fillRect);
+     struct NVGcontext *vg = (struct NVGcontext*)hdc;
+
+    int bdr_top     = 0;
+    int bdr_bottom  = 0;
+    int bdr_left    = 0;
+    int bdr_right   = 0;
+
+    if(borders.top.width != 0 && borders.top.style > litehtml::border_style_hidden)
+    {
+        bdr_top = (int) borders.top.width;
     }
+    if(borders.bottom.width != 0 && borders.bottom.style > litehtml::border_style_hidden)
+    {
+        bdr_bottom = (int) borders.bottom.width;
+    }
+    if(borders.left.width != 0 && borders.left.style > litehtml::border_style_hidden)
+    {
+        bdr_left = (int) borders.left.width;
+    }
+    if(borders.right.width != 0 && borders.right.style > litehtml::border_style_hidden)
+    {
+        bdr_right = (int) borders.right.width;
+    }
+
+    // TODO
+    // draw right border
+    // draw left border
+    // draw top border
+    // draw_bottom
+
+    // nvgSave(vg);
+    // nvgBeginPath(vg);
+    // nvgRoundedRect(vg, 0,0, 800, 600, 3.0f);
+    // nvgFillColor(vg, nvgRGBA(255,255,255,255));
+    // nvgFill(vg);
+    // nvgRestore(vg);
+    // drawWindow(vg, "window", 0.0f, 0.0f, 800.0f, 600.0f);
+    // LOGD("draw_borders: top %d bottom %d left %d right %d", bdr_top, bdr_bottom, bdr_left, bdr_right);
 }
 
 void container_sdl::transform_text(litehtml::tstring &text,
@@ -311,7 +341,6 @@ void container_sdl::open_page(const litehtml::tstring &url) {
     LOGD("html: %s", html.c_str());
     m_html = litehtml::document::createFromString(html.c_str(), this,
                                                   m_html_context);
-    LOGD("create document end");
     if (m_html) {
         LOGD("render document");
         m_html->render(800);
@@ -319,13 +348,13 @@ void container_sdl::open_page(const litehtml::tstring &url) {
     }
 }
 
-void container_sdl::on_draw() {
+void container_sdl::on_draw(struct NVGcontext *vg) {
     if (m_html) {
         litehtml::position pos;
         pos.width = 800;
         pos.height = 600;
         pos.x = 0;
         pos.y = 0;
-        m_html->draw((litehtml::uint_ptr)NULL, 0, 0, &pos);
+        m_html->draw((litehtml::uint_ptr)vg, 0, 0, &pos);
     }
 }
--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,66 @@
+#include "logdef.h"
 #include "container_sdl.h"
+
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+#ifdef NANOVG_GLEW
+#  include <GL/glew.h>
+#endif
+#include <GL/gl.h>
+#ifdef __cplusplus
+}
+#endif
+
 #include <SDL2/SDL.h>
+#include <SDL2/SDL_opengl.h>
 #include <SDL2/SDL_image.h>
 #include <SDL2/SDL_ttf.h>
+
+#include "nanovg.h"
+#define NANOVG_GLES2_IMPLEMENTATION
+#include "nanovg_gl.h"
+
 
 char master_css[] = {
 #include "master.css.inc"
     , 0};
+
+int loadFonts(NVGcontext* vg)
+{
+    int i;
+
+    if (vg == NULL)
+        return -1;
+
+    //data->images[i] = nvgCreateImage(vg, file, 0);
+
+    int fontIcons = nvgCreateFont(vg, "icons", "../nanovg/example/entypo.ttf");
+    if (fontIcons == -1) {
+        LOGE("Could not add font icons.");
+        return -1;
+    }
+    int fontNormal = nvgCreateFont(vg, "sans", "../nanovg/example/Roboto-Regular.ttf");
+    if (fontNormal == -1) {
+        LOGE("Could not add font italic.");
+        return -1;
+    }
+    int fontBold = nvgCreateFont(vg, "sans-bold", "../nanovg/example/Roboto-Bold.ttf");
+    if (fontBold == -1) {
+        LOGE("Could not add font bold.");
+        return -1;
+    }
+    int fontEmoji = nvgCreateFont(vg, "emoji", "../nanovg/example/NotoEmoji-Regular.ttf");
+    if (fontEmoji == -1) {
+        LOGE("Could not add font emoji.");
+        return -1;
+    }
+    nvgAddFallbackFontId(vg, fontNormal, fontEmoji);
+    nvgAddFallbackFontId(vg, fontBold, fontEmoji);
+
+    return 0;
+}
 
 int main(int argc, char *argv[]) {
     SDL_Window *win = NULL;
@@ -17,16 +72,44 @@ int main(int argc, char *argv[]) {
     SDL_Init(SDL_INIT_VIDEO);
 
     win = SDL_CreateWindow("Hello World", posX, posY, width, height, SDL_WINDOW_OPENGL);
+    //renderer = SDL_CreateRenderer(win, -1, SDL_RENDERER_ACCELERATED);
+    SDL_GL_SetAttribute( SDL_GL_CONTEXT_MAJOR_VERSION, 2 );
+    SDL_GL_SetAttribute( SDL_GL_CONTEXT_MINOR_VERSION, 0 );
+    //SDL_GL_SetAttribute( SDL_GL_CONTEXT_FLAGS, SDL_GL_CONTEXT_FORWARD_COMPATIBLE_FLAG ); // May be a performance booster in *nix?
+    SDL_GL_SetAttribute( SDL_GL_CONTEXT_PROFILE_MASK, SDL_GL_CONTEXT_PROFILE_CORE );
+    SDL_GL_SetAttribute( SDL_GL_STENCIL_SIZE, 8 );
+    SDL_GLContext glcontext = SDL_GL_CreateContext( win );
+    // glEnable(GL_STENCIL_TEST);
+  
+  struct NVGcontext *vg = NULL;
+  //struct NVGLUframebuffer *fb = NULL;
+ #ifdef NANOVG_GLEW
+    if(glewInit() != GLEW_OK) {
+        LOGE("Could not init glew.");
+        SDL_DestroyWindow(win);
+        return -1;
+    }
+#endif
 
-    renderer = SDL_CreateRenderer(win, -1, SDL_RENDERER_ACCELERATED);
-
-    bitmapSurface = SDL_LoadBMP("img/hello.bmp");
-    bitmapTex = SDL_CreateTextureFromSurface(renderer, bitmapSurface);
-    SDL_FreeSurface(bitmapSurface);
+  LOGD( "Creating nanovg context" );
+  vg = nvgCreateGLES2(NVG_ANTIALIAS|NVG_STENCIL_STROKES);
+  if ( !vg ) {
+    LOGE( "Couldn't create nanovg gl2 context" );
+    // Couldn't create context
+    SDL_DestroyWindow(win);
+    return 2;
+  } else {
+    if (loadFonts(vg)!=0) {
+        LOGE( "Couldn't create nanovg font context" );
+        // Couldn't create context
+        SDL_DestroyWindow(win);
+        return 2;
+    }
+    }
 
     litehtml::context html_context;
     html_context.load_master_stylesheet(master_css);
-    container_sdl *csdl = new container_sdl(&html_context, renderer);
+    container_sdl *container = new container_sdl(&html_context, vg);
     bool running = true;
     while (running) {
         SDL_Event e;
@@ -38,7 +121,7 @@ int main(int argc, char *argv[]) {
                                       "github.com/alenstar/litebrowser-sdl/"
                                       "build/index.html";
                     // std::string url = "http://www.litehtml.com/";
-                    csdl->open_page(url);
+                    container->open_page(url);
                     break;
                 }
                 case SDLK_ESCAPE:
@@ -50,15 +133,31 @@ int main(int argc, char *argv[]) {
                 break;
             }
         }
-        SDL_RenderClear(renderer);
-        csdl->on_draw(renderer);
         // SDL_RenderClear(renderer);
-        // SDL_RenderCopy(renderer, bitmapTex, NULL, NULL);
-        SDL_RenderPresent(renderer);
-    }
+        // container->on_draw(renderer);
+        // // SDL_RenderClear(renderer);
+        // // SDL_RenderCopy(renderer, bitmapTex, NULL, NULL);
+        // SDL_RenderPresent(renderer);
+        
+        // glClear(GL_COLOR_BUFFER_BIT|GL_DEPTH_BUFFER_BIT|GL_STENCIL_BUFFER_BIT);
+        glClearColor(1,1,1,1);
+        glClear(GL_COLOR_BUFFER_BIT|GL_DEPTH_BUFFER_BIT|GL_STENCIL_BUFFER_BIT);
+        glEnable(GL_BLEND);
+        glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+        glEnable(GL_CULL_FACE);
+        glDisable(GL_DEPTH_TEST);
 
-    SDL_DestroyTexture(bitmapTex);
-    SDL_DestroyRenderer(renderer);
+        nvgBeginFrame(vg, width, height, (float)width/(float)height);
+        container->on_draw(vg);
+        nvgEndFrame(vg);
+
+        glEnable(GL_DEPTH_TEST);
+        SDL_GL_SwapWindow( win );
+    }
+    nvgDeleteGLES2( vg );
+    SDL_GL_DeleteContext(glcontext);  
+    // SDL_DestroyTexture(bitmapTex);
+    // SDL_DestroyRenderer(renderer);
     SDL_DestroyWindow(win);
 
     SDL_Quit();
